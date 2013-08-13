e92a54f21fd0e9a234d43852fffd878df7dc7061
Alistair Ward <AlistairNWard@gmail.com>
2013-08-13 09:40:33 -0400

    Version 0.01 - August 2013
    1. Moved pythin files into src directory.
    
    2. Modified name of function.

---
+++ src/version.py
@@ -0,0 +1,10 @@
+"""
+Version information for configurationClass, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__author__  = 'Alistair Ward'
+__version__ = '0.01'
+__date__    = 'August 2013'
