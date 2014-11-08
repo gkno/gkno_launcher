21ed5cd24c5c959c7e0a93becdb06b32919d8589
Alistair Ward <AlistairNWard@gmail.com>
2014-11-08 12:34:27 -0500

    Version 1.43.0 - November 2014
    1. Updated wham-bam.

--- src/gkno.py
+++ src/gkno.py
@@ -59,7 +59,7 @@ import gkno.writeToScreen
 from gkno.writeToScreen import *
 
 __author__ = "Alistair Ward"
-__version__ = "1.42.6"
+__version__ = "1.43.0"
 __date__ = "November 2014"
 
 def main():
