e877c38a3acf3ab54218c88082b112ed4f251150
Alistair Ward <AlistairNWard@gmail.com>
2014-11-13 13:18:14 -0500

    Version 1.48.3 - November 2014
    1. Updated argument order in bwa-index.

--- src/gkno.py
+++ src/gkno.py
@@ -59,7 +59,7 @@ import gkno.writeToScreen
 from gkno.writeToScreen import *
 
 __author__ = "Alistair Ward"
-__version__ = "1.48.2"
+__version__ = "1.48.3"
 __date__ = "November 2014"
 
 def main():
