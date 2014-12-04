54237c3df3f018bd58632e8f80a3d7b1d37c6f41
Alistair Ward <AlistairNWard@gmail.com>
2014-12-04 11:42:39 -0500

    Version 1.53.2 - December 2014
    1. Updated a couple of configuration files.
    
    2. Updated the R script for plotting Rufus data.

--- tools/R/rufus-trio-coverage.r
+++ tools/R/rufus-trio-coverage.r
@@ -3,43 +3,41 @@ library(reshape2)
 library(ggplot2)
 
 # Import data
-parent1 <- read.table("parent1.r.histo", header=FALSE)
-parent2 <- read.table("parent2.r.histo", header=FALSE)
-child   <- read.table("child.r.histo", header=FALSE)
+args   <- commandArgs(TRUE)
+mother <- read.table(args[1], header=FALSE)
+father <- read.table(args[2], header=FALSE)
+child  <- read.table(args[3], header=FALSE)
 
 # Calculate the coverage of all three samples.
-temp <- which(diff(sign(diff(parent1$V2)))==-2)+1
-parent1Coverage <- temp[2] - 1
+temp <- which(diff(sign(diff(mother$V2)))==-2)+1
+motherCoverage <- temp[2] - 1
 
-temp <- which(diff(sign(diff(parent2$V2)))==-2)+1
-parent2Coverage <- temp[2] - 1
+temp <- which(diff(sign(diff(father$V2)))==-2)+1
+fatherCoverage <- temp[2] - 1
 
 temp <- which(diff(sign(diff(child$V2)))==-2)+1
 childCoverage <- temp[2] - 1
 
 # Store the max coverage of all the samples.
-maxCoverage <- max(parent1Coverage, parent2Coverage, childCoverage)
+maxCoverage <- max(motherCoverage, fatherCoverage, childCoverage)
 
 # Determine the number of k-mers at the coverage peak and identify the max.
-parent1Count <- parent1$V2[parent1Coverage]
-parent2Count <- parent2$V2[parent2Coverage]
-childCount   <- child$V2[childCoverage]
-maxCount     <- max(parent1Count, parent2Count, childCount)
+motherCount <- mother$V2[motherCoverage]
+fatherCount <- father$V2[fatherCoverage]
+childCount  <- child$V2[childCoverage]
+maxCount    <- max(motherCount, fatherCount, childCount)
 
 # Set up the 'data' data frame to hold all data
-data <- parent1
+data <- mother
 
 # Rename the columns and add the parent2 and child data
-names(data)  <- c("Coverage", "Parent1")
-data$Parent2 <- parent2$V2
-data$Child   <- child$V2
+names(data) <- c("coverage", "mother")
+data$father <- father$V2
+data$child  <- child$V2
 
 # Melt the data into long format
-data.melt <- melt(data, id.vars = 'Coverage', variable.name = 'Sample', value.name = 'Count')
+data.melt <- melt(data, id.vars = 'coverage', variable.name = 'sample', value.name = 'count')
 
 # Plot the data
-ggplot(data.melt, aes(x = Coverage, y = Count, color = Sample)) +
-geom_line() +
-scale_color_manual(values = c('Parent1' = 'skyblue', 'Parent2' = 'skyblue4', 'Child' = 'indianred')) +
-xlim(0, 1.5 * maxCoverage) + 
-ylim(0, 1.02 * maxCount)
+pdf(args[4])
+ggplot(data.melt, aes(x = coverage, y = count, color = sample)) + geom_line() + scale_color_manual(values = c('mother' = 'skyblue', 'father' = 'skyblue4', 'child' = 'indianred')) + xlim(0, 1.5 * maxCoverage) + ylim(0, 1.02 * maxCount) + theme_bw(12)
