69a52fddae3b6770dbfe0319d9c8a0af6d55b2f7
Alistair Ward <AlistairNWard@gmail.com>
2014-12-07 15:59:29 -0500

    Version 1.56.0 - December 2014
    1. Updated Rufus. Require a check on the number of supplied arguments.

--- src/gkno.py
+++ src/gkno.py
@@ -59,7 +59,7 @@ import gkno.writeToScreen
 from gkno.writeToScreen import *
 
 __author__ = "Alistair Ward"
-__version__ = "1.55.0"
+__version__ = "1.56.0"
 __date__ = "November 2014"
 
 def main():
