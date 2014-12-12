be2ddf88910ed00dd354c74d41965d3278b95ea0
Alistair Ward <AlistairNWard@gmail.com>
2014-12-12 13:00:08 -0500

    Version 1.57.1 - December 2014
    1. Updated coverage pipeline and tools.
    
    2. Updated help messages in some tools and pipelines.

--- tools/R/plot-histogram.r
+++ tools/R/plot-histogram.r
@@ -8,9 +8,13 @@ hist.melted <- melt(hist, id = "bin")
 min <- 0
 max <- 0
 for (i in names(hist)[-1]) {
-  hist2 <- rep(hist[,1], hist[,i])  ## expands the data by frequency of #score
+  hist2  <- rep(hist[,1], hist[,i])  ## expands the data by frequency of #score
   median <- median(hist2)
-  quant <- quantile(hist2, c(0.90))
+  quant  <- quantile(hist2, c(0.90))
+  
+  # Handle missing values
+  if (is.na(median)) { median <- 0 }
+  if (is.na(quant)) { quant <- 0 }
 
   # Update the min and max.
   if ( (median + quant) > max) {max <- (median + quant)}
@@ -20,4 +24,9 @@ for (i in names(hist)[-1]) {
 if (min < 0) {min <- 0}
                     
 pdf(args[2])
+
+# Use user defined values for the min and max x-values if provided.
+if (as.integer(args[6]) != -1) {min <- as.integer(args[6])}
+if (as.integer(args[7]) != -1) {max <- as.integer(args[7])}
+
 ggplot() + geom_line(data=hist.melted, aes(x=bin, y=value, group=variable, colour=variable)) + xlim(min, max) + xlab(args[3]) + ylab(args[4]) + theme_bw(12) + scale_colour_discrete(name=args[5])
