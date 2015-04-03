9562de7d7abbcc7ce859a498be4e560a4c1efbab
Alistair Ward <AlistairNWard@gmail.com>
2015-04-02 20:38:36 -0400

    Version branch.version2 0.106 - April 2015
    1. Updated test_harness.

--- test-harness/test-harness.py
+++ test-harness/test-harness.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import os.path
 import getpass
+import shutil
 import subprocess
 import sys
 
@@ -48,56 +49,108 @@ def main():
   print('Executing gkno test harness.', file = sys.stdout)
   print('============================', file = sys.stdout)
 
+  # Get a list of all files/directories that should not be deleted.
+  keep, copyFiles = getRequiredFiles()
+
   # Test all of the included pipelines.
-  testPipelines()
+  testPipelines(keep, copyFiles)
 
   # Check that help messages can be requested.
-  testCommands(0, 'gkno help errors', helpCommands())
+  testCommands(0, 'gkno help errors', helpCommands(), keep)
 
   # Test all of the predefined pipeline configuration files. These fall into a range of
   # different test cases.
-  testErrorCase(2, 'admin errors')
-  testErrorCase(3, 'command line errors')
-  testErrorCase(4, 'file handling errors')
-  testErrorCase(5, 'general configuration file errors')
-  testErrorCase(6, 'tool configuration file errors')
-  testErrorCase(7, 'pipeline configuration file errors')
-  testErrorCase(8, 'graph construction errors')
-  testErrorCase(9, 'argument errors')
-  testCommands(10, 'malformed command line errors', userCommands())
-  testErrorCase(10, 'data consistency errors')
-  testErrorCase(11, 'help request errors')
-  testErrorCase(12, 'plotting graph errors')
-  testErrorCase(13, 'makefile generation errors')
+  testErrorCase(2, 'admin errors', keep)
+  testErrorCase(3, 'command line errors', keep)
+  testErrorCase(4, 'file handling errors', keep)
+  testErrorCase(5, 'general configuration file errors', keep)
+  testErrorCase(6, 'tool configuration file errors', keep)
+  testErrorCase(7, 'pipeline configuration file errors', keep)
+  testErrorCase(8, 'graph construction errors', keep)
+  testErrorCase(9, 'argument errors', keep)
+  testCommands(10, 'malformed command line errors', userCommands(), keep)
+  testErrorCase(10, 'data consistency errors', keep)
+  testErrorCase(11, 'help request errors', keep)
+  testErrorCase(12, 'plotting graph errors', keep)
+  testErrorCase(13, 'makefile generation errors', keep)
+
+# Read the keep-files.txt file and store a list of files/directories that should not be deleted.
+# In addition, read the copy-files.txt file and determine the files that need to be copied to the
+# test harness before each task. Ensure that these files exist prior to executing the tests.
+def getRequiredFiles():
+  filehandle   = open('keep-list.txt')
+  isTerminate  = False
+  keep         = []
+  resourcePath = '/d1/home/award/gkno/temp/gkno_launcher/tutorial/'
+  for line in filehandle.readlines(): keep.append(line.rstrip('\n'))
+
+  filehandle = open('copy-files.txt')
+  copy       = {}
+  for line in filehandle.readlines():
+    task  = line.split(':')[0].replace('"', '').replace(' ', '')
+    files = line.split(':')[1].rstrip('\n').split(',')
+
+    # Store the filenames.
+    copy[task] = []
+    for filename in files:
+      updatedFilename = str(resourcePath + filename.replace('"', '').replace(' ', ''))
+      if not os.path.isfile(updatedFilename): isTerminate = True
+      else: copy[task].append(updatedFilename)
+
+  # Terminate if not all the files exist.
+  if isTerminate:
+    print('Ensure all files in copy-files.txt are in the resources directory.')
+    exit(1)
+
+  return keep, copy
 
 # Loop over all of the pipelines included with gkno and check that they all execute when provided
 # with the test parameter set.
-def testPipelines():
+def testPipelines(keep, copy):
 
   # Define a test class.
   harness = testHarness(0)
 
   # Print to screen the tests being performed.
   print(file = sys.stdout)
-  print('Testing defined pipelines...', end = '', file = sys.stdout)
+  print('Testing defined pipelines...', file = sys.stdout)
   sys.stdout.flush()
 
   # Loop over all defined pipelines.
   for filename in os.listdir('../config_files/pipes'):
     if filename.endswith('.json') and not filename.endswith('parameter-sets.json'):
       pipeline = filename.rsplit('.json')[0]
+      print('\t', pipeline, '...', sep = '', end = '', file = sys.stdout)
+      sys.stdout.flush()
+
+      # Copy across any necessary files.
+      copyFiles(copy, pipeline)
+
+      # Execute the command.
       command  = harness.executable + ' ' + pipeline + ' -ps test'
       success  = subprocess.call(command.split(), stdout = harness.devnull, stderr = harness.devnull)
-      if success == harness.errorCode: harness.successes += 1
-      else: harness.failures.append(str(pipeline + '. Command:'  + command))
+      if success == harness.errorCode:
+        harness.successes += 1
+        print('succeeded.', file = sys.stdout)
+        sys.stdout.flush()
+      else:
+        harness.failures.append(str(pipeline + '. Command: '  + command))
+        print('failed.', file = sys.stdout)
+        sys.stdout.flush()
+
+      # Delete created files.
+      deleteFiles(keep)
 
   # Print the results.
-  print('complete.', file = sys.stdout)
-  sys.stdout.flush()
   harness.printResults()
 
+# Copy across necessary files.
+def copyFiles(copy, pipeline):
+  if pipeline in copy:
+    for filename in copy[pipeline]: shutil.copy(filename, '.')
+
 # Check help messages.
-def testCommands(code, text, commands):
+def testCommands(code, text, commands, keep):
 
   # Get the gkno executable.
   harness = testHarness(code)
@@ -113,13 +166,16 @@ def testCommands(code, text, commands):
     if success == code: harness.successes += 1
     else: harness.failures.append(str(name + '. Command: ' + command))
 
+    # Delete created files.
+    deleteFiles(keep)
+
   # Print the results.
   print('complete.', file = sys.stdout)
   sys.stdout.flush()
   harness.printResults()
 
 # Check all instances of error and ensure that gkno terminates with the correct error code.
-def testErrorCase(code, text):
+def testErrorCase(code, text, keep):
 
   # Define a test class.
   harness = testHarness(code)
@@ -136,7 +192,10 @@ def testErrorCase(code, text):
     command  = harness.executable + ' ' + pipeline + ' -cp ' + path
     success  = subprocess.call(command.split(), stdout = harness.devnull, stderr = harness.devnull)
     if success == harness.errorCode: harness.successes += 1
-    else: harness.failures.append(str(pipeline + '. Command:'  + command))
+    else: harness.failures.append(str(pipeline + '. Command: '  + command))
+
+    # Delete created files.
+    deleteFiles(keep)
 
   # Print the results.
   print('complete.', file = sys.stdout)
@@ -183,6 +242,13 @@ def userCommands():
              ]
 
   return commands
+
+# Delete all created files.
+def deleteFiles(keep):
+  for filename in os.listdir('.'):
+    if filename not in keep and not filename.startswith('.'):
+      if os.path.isfile(filename): os.remove(filename)
+      elif os.path.isdir(filename): shutil.rmtree(filename)
 
 if __name__ == "__main__":
   main()
