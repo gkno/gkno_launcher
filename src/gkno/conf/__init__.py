56773284ab0e1aeddefeb15b991f0bf51ff72e4f
Alistair Ward <wardag@bc.edu>
2015-04-09 17:40:33 -0400

    Version 1.61.1 - April 2015
    1. Remove some pyc files in preparation for version2 merge.

--- src/gkno/conf/__init__.py
+++
@@ -1,7 +0,0 @@
-import gkno.conf.gknoDependencies
-import gkno.conf.gknoResources
-import gkno.conf.gknoTools
-
-dependencies = gknoDependencies.List
-resources    = gknoResources.List
-tools        = gknoTools.List
