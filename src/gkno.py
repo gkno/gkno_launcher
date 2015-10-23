afbada304b0d852738b628a5a528dff96364aa7b
Alistair Ward <wardag@bc.edu>
2015-10-23 11:53:22 -0400

    Version 2.34.0 - October 2015
    1. Updated bwa and bwa-se pipielines to set the reference short-form
    argument to -r instead of -rp to avoid conflict with the remove
    parameter set argument.

--- src/gkno.py
+++ src/gkno.py
@@ -32,8 +32,8 @@ import gkno.web as w
 import gkno.writeToScreen as write
 
 __author__ = "Alistair Ward"
-__version__ = "2.33.5"
-__date__ = "September 2015"
+__version__ = "2.34.0"
+__date__ = "October 2015"
 
 def main():
 
